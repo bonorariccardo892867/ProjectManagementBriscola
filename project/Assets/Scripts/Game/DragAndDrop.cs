589cfb1e5d876f3be5abe84fdb0a2376137fbb59
B0scar <oscargalacsi2@gmail.com>
2024-04-20 23:06:38 +0200

    implemented dropzones
    vifs6fguwevyagnfweigvt7wu

--- project/Assets/Scripts/Game/DragAndDrop.cs
+++ project/Assets/Scripts/Game/DragAndDrop.cs
@@ -5,17 +5,48 @@ using UnityEngine;
 public class DragAndDrop : MonoBehaviour
 {
     public GameObject Canvas;
+    public GameObject  DropZone;
     private bool isDragging = false;
+    private GameObject startParent;
+    private Vector2 startposition;
+    private GameObject dropZone;
+    private bool isOverDropZone;
     // Start is called before the first frame update
     void Start()
     {
         Canvas = GameObject.Find("Main Canvas");
+        DropZone = GameObject.Find("DropZone");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("colliding!");
+        isOverDropZone = true;
+        dropZone = collision.gameObject;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        Debug.Log("uncolliding!");
+        isOverDropZone = false;
+        dropZone = null;
     }
     public void StartDrag(){
         isDragging=true;
+        startParent = transform.parent.gameObject;
+        startposition = transform.position;
+
     }
     public void EndDrag(){
         isDragging=false;
+        if (isOverDropZone)
+        {
+            transform.SetParent(dropZone.transform, false);
+        }else
+        {
+            transform.position = startposition;
+            transform.SetParent(startParent.transform,false);
+        }
     }
     // Update is called once per frame
     void Update()
