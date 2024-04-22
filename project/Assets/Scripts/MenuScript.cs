96347918cdf38e645eda82d43e07a1941305e9a4
Federico <100353451+fediiiiiiiii@users.noreply.github.com>
2024-04-22 23:52:43 +0200

    Comments added

--- project/Assets/Scripts/MenuScript.cs
+++ project/Assets/Scripts/MenuScript.cs
@@ -5,12 +5,15 @@ using Mirror;
 
 public class MenuScript : MonoBehaviour
 {
-
+    // NetworkManager
     public NetworkManager networkManager;
+
+    // UI panels
     public GameObject menuPanel;
     public GameObject gamePanel;
     public GameObject joinPanel;
 
+    // Function to start hosting a game
     public void Host()
     {
         networkManager.StartHost();
@@ -19,17 +22,20 @@ public class MenuScript : MonoBehaviour
         gamePanel.SetActive(true);
     }
 
+    // Function to set IP address for joining a game
     public void SetIP(string ip)
     {
         networkManager.networkAddress = ip;
     }
 
+    // Function to switch to the join menu
     public void JoinMenu(){
         menuPanel.SetActive(false);
         joinPanel.SetActive(true);
         gamePanel.SetActive(false);
     }
 
+    // Function to join a game
     public void Join()
     {
         networkManager.StartClient();
@@ -38,20 +44,24 @@ public class MenuScript : MonoBehaviour
         gamePanel.SetActive(true);
     }
 
+    // Function to go back to the main menu
     public void Back(){
         joinPanel.SetActive(false);
         menuPanel.SetActive(true);
         gamePanel.SetActive(false);
     }
 
+    // Function to stop hosting or disconnect from the server
     public void Stop()
     {
         if (networkManager.mode == NetworkManagerMode.Host)
         {
+            // If hosting, stop hosting
             networkManager.StopHost();
         }
         if (networkManager.mode == NetworkManagerMode.ClientOnly)
         {
+            // If client, stop client
             networkManager.StopClient();
         }
     }
@@ -67,11 +77,12 @@ public class MenuScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Check if server or client is active
         if (NetworkServer.active || NetworkClient.active)
         {
             gamePanel.SetActive(true);
-        }
-        else if(joinPanel.activeSelf)
+        } // If join panel is active
+        else if(joinPanel.activeSelf) 
         {
             gamePanel.SetActive(false);
             menuPanel.SetActive(false);
