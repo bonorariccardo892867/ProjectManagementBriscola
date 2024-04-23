7d93df2195814515c87e4f57de4564dfaf705be1
bonorariccardo892867 <100940685+bonorariccardo892867@users.noreply.github.com>
2024-04-23 12:44:23 +0200

    Continuation of Part 8 - does not build
    Continuation of Part 8 - it does not currently build properly

--- project/Assets/Scripts/DrawCards.cs
+++ project/Assets/Scripts/DrawCards.cs
@@ -1,26 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class DrawCards : MonoBehaviour
+public class DrawCards : NetworkBehaviour
 {
+    public PlayerManager playerManager;
     public GameObject Card1;
     public GameObject PlayerArea;
     // Start is called before the first frame update
     public void OnCLick(){
-        for (int i=0;i<5;i++){
-            GameObject card = Instantiate(Card1,new Vector2(0,0),Quaternion.identity);
-            card.transform.SetParent(PlayerArea.transform,false);
-        }
-    }
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        NetworkIdentity networkIdentity = NetworkClient.connection.identity;
+        playerManager = networkIdentity.GetComponent<PlayerManager>();
+        playerManager.CmdDealCards();
     }
 }
--- project/Assets/Scripts/PlayerManager.cs
+++ project/Assets/Scripts/PlayerManager.cs
@@ -10,7 +10,7 @@ public class PlayerManager : NetworkBehaviour {
     public GameObject EnemyArea;
     public GameObject DropZone;
 
-    List <GameObject> card = new List<GameObject>();
+    List <GameObject> cards = new List<GameObject>();
 
     public override void OnStartClient(){
         base.OnStartClient();
@@ -20,6 +20,22 @@ public class PlayerManager : NetworkBehaviour {
     }
     [Server]
     public override void OnStartServer(){
-        card.Add(card1);
+        cards.Add(card1);
+    }
+    [Command]
+    public void CmdDealCards(){
+        for (int i=0;i<5;i++){
+            GameObject card = Instantiate(cards[Random.Range(0,cards.Count)],new Vector2(0,0),Quaternion.identity);
+            NetworkServer.Spawn(card,connectionToClient);
+            RpcShowCard(card,"dealt");
+        }
+    }
+    [ClientRpc]
+    private void RpcShowCard(GameObject card, string type){
+        if (type == "dealt"){
+            card.transform.SetParent(PlayerArea.transform,false);
+        }else if (type == "played"){
+            card.transform.SetParent(EnemyArea.transform,false);
+        }
     }
 }
